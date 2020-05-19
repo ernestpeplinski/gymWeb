b01628bfb14c19d2cbe501e6ab1ee208f7416c1c
pepern <ernestpeplinski@gmail.com>
2020-05-19 19:18:24 +0200

    add elements

--- src/app/model/gym-excercises.ts
+++ src/app/model/gym-excercises.ts
@@ -1,52 +1,52 @@
 export class GymExcercises{
-    public categories = 
+    public categories =
     `[{
         "name":"Barki",
         "excercises": [
             {
-               "name":"Cwiczenie 1 na Silowni",
-               "description":"to jest jakies jebane cwiczenie"
+               "name":"Unoszenie ramienia przy wyciągu dolnym",
+               "description":"Stań tyłem do wyciągu dolnego w lekkim rozkroku i chwyć jedną ręką jego linkę. Drugą rękę oprzyj na biodrze. Z tej pozycji wykonaj wznos wyprostowanego ramienia przed siebie, do góry, do linii barków. Wstrzymaj na chwilę ruch i powoli wróć do pozycji wyjściowej. "
             },
             {
-                "name":"Cwiczenie 2 na Silowni",
-                "description":"to jest jakies jebane cwiczenie"
+                "name":"Unoszenie ramienia w bok stojąc",
+                "description":"Stań w lekkim rozkroku. Chwyć sztangielkę i przy lekko zgiętym ramieniu trzymaj wzdłuż tułowia. Drugą rękę oprzyj na biodrze. Z tej pozycji unieś ramię szerokim bocznym łukiem do linii barków. Wstrzymaj na chwilę ruch, a następnie wróć do pozycji wyjściowej."
              },
              {
-                "name":"Cwiczenie 3 na Silowni",
-                "description":"to jest jakies jebane cwiczenie"
+                "name":"Wznosy ramion na boki przy wyciągu",
+                "description":"Stań w lekkim rozkroku (nogi na szerokość barków), krzyżujące się linki wyciągu dolnego trzymaj w wyprostowanych ramionach wzdłuż tułowia. Z tej pozycji szerokim łukiem wykonaj wznos ramion do linii barków. Wstrzymaj na chwilę ruch i wróć do pozycji wyjściowej."
              },
              {
-                "name":"Cwiczenie 4 na Silowni",
-                "description":"to jest jakies jebane cwiczenie"
+                "name":"Wznosy ramion w opadzie tułowia",
+                "description":"Stań w lekkim rozkroku ze sztangielkami trzymanymi przed sobą chwytem neutralnym. Wykonaj opad tułowia, tak aby był on równoległy do podłoża, a ramiona swobodnie zwisały. Unieś sztangielki szerokim łukiem do linii barków. Wstrzymaj na chwilę ruch i powoli opuść do pozycji wyjściowej. Ramiona powinny być wyprostowane w łokciach."
              },
              {
-                "name":"Cwiczenie 5 na Silowni",
-                "description":"to jest jakies jebane cwiczenie"
+                "name":"Wznosy ramion na ławce skośnej",
+                "description":"Usiądź na ławeczce skośnej (kąt nachylenia oparcia około 45 stopni), opierając brzuch i klatkę o ławeczkę, głowa lekko uniesiona. Sztangielki trzymaj w wyprostowanych ramionach wzdłuż tułowia przed sobą. Z tej pozycji szerokim łukiem wykonaj wznos ramion do góry do linii barków. Wstrzymaj na chwilę ruch i wróć do pozycji wyjściowej."
              }
         ]
     },
     {
-        "name":"Klata",
+        "name":"Klata piersiowa",
         "excercises": [
             {
-               "name":"Cwiczenie 1 na Silowni",
-               "description":"to jest jakies jebane cwiczenie"
+               "name":"Pompki na poręczach",
+               "description":"Połóż dłonie na poręczach równolegle do siebie. Ramiona trzymaj wyprostowane, ale nie zablokowane w łokciach. Mając nogi lekko ugięte w kolanach, powoli opuść się na tyle, abyś mógł unieść korpus i wykonać pompkę. Im niżej się opuścisz, tym mocniej będą pracowały mięśnie klatki piersiowej. Ale musisz się jeszcze podnieść."
             },
             {
-                "name":"Cwiczenie 2 na Silowni",
-                "description":"to jest jakies jebane cwiczenie"
+                "name":"Rozpiętki na ławce poziomej",
+                "description":"Połóż się na ławeczce poziomej na plecach. Weź w każdą rękę sztangielkę i trzymaj je w wyprostowanych ramionach, równolegle do siebie i złączone nad klatką piersiową. Sztangielki powinny być lżejsze od tych, których używasz przy wyciskaniu. Zaczynając z tej pozycji, szerokim łukiem opuść sztangielki na boki. Ramiona trzymaj lekko ugięte w łokciach. Maksymalnie rozciągnij klatkę piersiową i powoli powróć do pozycji wyjściowej."
              },
              {
-                "name":"Cwiczenie 3 na Silowni",
-                "description":"to jest jakies jebane cwiczenie"
+                "name":"Ściąganie linek wyciągu górnego",
+                "description":"Stań w bramie (bokiem między wyciągami), tułów lekko pochyl, nogi ugięte i rozstawione na szerokość barków. Z uchwytem wyciągów górnych w każdej dłoni, unieś ramiona, dłonie skieruj wewnętrzną stroną w kierunku podłogi i lekko zegnij ramiona w łokciach. Z tej pozycji powoli szerokim łukiem opuść ramiona, aż do skrzyżowania ich przed sobą. Następnie powoli wracaj do pozycji wyjściowej."
              },
              {
-                "name":"Cwiczenie 4 na Silowni",
-                "description":"to jest jakies jebane cwiczenie"
+                "name":"Wyciskanie hantli na ławce z głową w dół",
+                "description":"Połóż się na ławeczce skośnej na plecach głową w dół i chwyć sztangielki nachwytem. Sztangielki trzymaj w jednej linii na lekko ugiętych ramionach, złączone nad klatką piersiową. Powoli opuść je, odwodząc łokcie na bok, tak by znalazły się jak najniżej. Potem powoli wróć do pozycji wyjściowej. Zwróć szczególną uwagę na fazę opuszczania sztangielek nad klatką piersiową - rozpocznij to ćwiczenie od mniejszych ciężarów. Gdy prawidłowo będziesz je wykonywał, możesz zwiększyć ciężar."
              },
              {
-                "name":"Cwiczenie 5 na Silowni",
-                "description":"to jest jakies jebane cwiczenie"
+                "name":"Wyciskanie sztangi na ławce skośnej",
+                "description":"Połóż się na ławce na plecach i chwyć sztangę nachwytem szerzej niż szerokość barków. Sztang ę na lekko ugiętych ramionach trzymaj nad klatką piersiową. Powoli ją opuść, a potem wróć do pozycji wyjściowej."
              }
         ]
     },
